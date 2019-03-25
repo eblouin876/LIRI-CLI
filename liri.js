86d630625d1c68befa1f6695e3b8c80bad2159c2
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-03-25 14:25:28 -0700

    Built basic checking logic for initial inquirere

--- liri.js
+++ liri.js
@@ -4,3 +4,32 @@ let keys = require("./keys.js");
 let movie = require("./movie.js")
 let spotify = require("./spotify.js")
 let conert = require("./concert.js")
+
+
+
+inquirer
+    .prompt([{
+        name: 'selection',
+        type: 'checkbox',
+        choices: [{
+            name: 'Look up a concert',
+            value: 'concert'
+        }, {
+            name: 'Look up a song',
+            value: 'spotify'
+        }, {
+            name: 'Look up a movie',
+            value: 'movie'
+        }, {
+            name: 'Look up random',
+            value: 'random'
+        }]
+    }])
+    .then(res => {
+        res.selection.forEach(query => {
+            if (query === 'conert') {}
+            if (query === 'spotify') {}
+            if (query === 'movie') {}
+            if (query === 'random') {}
+        })
+    })
